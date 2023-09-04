d4161fb6edae90fd1f018fb8addfc636c6024bc4
Christian Miguez <kisquian@gmail.com>
2023-09-04 09:35:23 -0300

    prisma added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,9 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 // import { MongoDBAdapter } from "@auth/mongodb-adapter";
 // import clientPromise from "@/lib/mongodb.adapter";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma.adapter";
 
 const handler = NextAuth({
   providers: [
